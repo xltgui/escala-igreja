90b41cd2751cd84872d2aa4f72757a22888f48d4
xltgui <borba.liver@gmail.com>
2025-08-20 23:19:01 -0300

    test

--- src/main/java/com/github/xltgui/escalaigreja/security/CustomUserDetailsService.java
+++ src/main/java/com/github/xltgui/escalaigreja/security/CustomUserDetailsService.java
@@ -15,7 +15,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) {
         UserEntity userEntity = service.findByUsername(username);
-        System.out.println("USER");
         return User.
                 builder()
                 .username(username)
